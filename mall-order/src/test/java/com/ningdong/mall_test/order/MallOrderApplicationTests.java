9f4c8cfb4e9fbfeaaf16b870fccf2b8157cf15bf
1225235462 <a1225235462@gmail.com>
2023-09-25 17:45:33 +0800

    项目创始结构创建

---
+++ mall-order/src/test/java/com/ningdong/mall_test/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ningdong.mall_test.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/main/java/com/ningdong/mall_test/ware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package com.ningdong.mall_test.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
