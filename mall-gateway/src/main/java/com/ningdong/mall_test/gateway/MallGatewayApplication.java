1ec6c4809c34bd7c50bef00ae6275e529875a673
1225235462 <a1225235462@gmail.com>
2023-09-27 18:43:50 +0800

    第三天学习2

--- mall-gateway/src/main/java/com/ningdong/mall_test/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/ningdong/mall_test/gateway/MallGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ningdong.mall_test.coupon;
+package com.ningdong.mall_test.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MallCouponApplication {
+public class MallGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallCouponApplication.class, args);
+        SpringApplication.run(MallGatewayApplication.class, args);
     }
 
 }
--- mall-member/src/main/java/com/ningdong/mall_test/member/controller/MemberController.java
+++ mall-member/src/main/java/com/ningdong/mall_test/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ningdong.mall_test.member.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.ningdong.mall_test.member.feign.CouponFeignService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,19 @@ import com.ningdong.common.utils.R;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setNickname("ning");
+
+        R membercoupons = couponFeignService.membercoupons();
+
+        return R.ok().put("member",memberEntity).put("coupons",membercoupons.get("coupons"));
+    }
 
     /**
      * 列表
