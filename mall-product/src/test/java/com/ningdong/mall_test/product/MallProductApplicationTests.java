d163816d32b6134b3d7ee99bbad7d2c96276a177
1225235462 <a1225235462@gmail.com>
2023-09-27 10:36:42 +0800

    第三天学习1补充

--- mall-product/src/test/java/com/ningdong/mall_test/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/ningdong/mall_test/product/MallProductApplicationTests.java
@@ -1,10 +1,13 @@
 package com.ningdong.mall_test.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ningdong.mall_test.product.entity.BrandEntity;
 import com.ningdong.mall_test.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
@@ -14,11 +17,21 @@ class MallProductApplicationTests {
 
     @Test
     void contextLoads() {
-        BrandEntity brandEntity = new BrandEntity();
+//        BrandEntity brandEntity = new BrandEntity();
 
-        brandEntity.setName("索尼");
-        brandService.save(brandEntity);
-        System.out.println("success!");
+//        brandEntity.setName("索尼");
+//        brandService.save(brandEntity);
+//        System.out.println("success!");
+
+//        brandEntity.setBrandId(6L);
+//        brandEntity.setDescript("信仰");
+
+//        brandService.updateById(brandEntity);
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",6L));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
