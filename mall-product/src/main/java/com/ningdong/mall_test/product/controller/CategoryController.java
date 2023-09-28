c236075b2d08f552c6eaf2fd64df86bef51f50c8
1225235462 <a1225235462@gmail.com>
2023-09-28 13:10:35 +0800

    第四天学习1

--- mall-product/src/main/java/com/ningdong/mall_test/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/ningdong/mall_test/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ningdong.mall_test.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -30,6 +31,13 @@ import com.ningdong.common.utils.R;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    //获取菜单树
+    @RequestMapping("/list/tree")
+    public R listTree(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data",entities);
+    }
 
     /**
      * 列表
--- mall-product/src/main/java/com/ningdong/mall_test/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/ningdong/mall_test/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.ningdong.mall_test.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +31,26 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    //菜单树方法实现
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0).map(menu -> {
+            menu.setChildren(getChildens(menu, entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(CategoryEntity::getSort)).collect(Collectors.toList());
+
+
+        return level1Menus;
+    }
+
+    //获取父菜单的子菜单
+    private List<CategoryEntity> getChildens(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> collect = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId()).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildens(categoryEntity, all));
+            return categoryEntity;
+        }).sorted(Comparator.comparingInt(CategoryEntity::getSort)).collect(Collectors.toList());
+        return collect;
+    }
 }
