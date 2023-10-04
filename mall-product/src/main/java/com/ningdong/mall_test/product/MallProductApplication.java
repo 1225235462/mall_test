e9f129b2585ff9c53ba664caaf9dba2f1d8b8135
1225235462 <a1225235462@gmail.com>
2023-10-04 17:33:35 +0800

    第五天学习

--- mall-product/src/main/java/com/ningdong/mall_test/product/MallProductApplication.java
+++ mall-product/src/main/java/com/ningdong/mall_test/product/MallProductApplication.java
@@ -3,9 +3,11 @@ package com.ningdong.mall_test.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.ningdong.mall_test.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {
