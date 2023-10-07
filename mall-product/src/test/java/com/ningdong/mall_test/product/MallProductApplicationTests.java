9a3e7763fbdcb9370e9dd592fb655cd9a761d379
1225235462 <a1225235462@gmail.com>
2023-10-07 17:58:42 +0800

    第八天学习

--- mall-product/src/test/java/com/ningdong/mall_test/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/ningdong/mall_test/product/MallProductApplicationTests.java
@@ -1,37 +1,80 @@
 package com.ningdong.mall_test.product;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.ningdong.mall_test.product.entity.BrandEntity;
-import com.ningdong.mall_test.product.service.BrandService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+//import com.aliyun.oss.common.auth.*;
+//import com.aliyun.oss.model.PutObjectRequest;
+//import com.aliyun.oss.model.PutObjectResult;
+//
+//import java.io.FileInputStream;
+//import java.io.InputStream;
 
-import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
-    @Autowired
-    BrandService brandService;
+//    @Autowired
+//    BrandService brandService;
+//
+//    @Test
+//    void contextLoads() {
+//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",6L));
+//        list.forEach((item)->{
+//            System.out.println(item);
+//        });
+//    }
 
-    @Test
-    void contextLoads() {
-//        BrandEntity brandEntity = new BrandEntity();
-
-//        brandEntity.setName("索尼");
-//        brandService.save(brandEntity);
-//        System.out.println("success!");
-
-//        brandEntity.setBrandId(6L);
-//        brandEntity.setDescript("信仰");
-
-//        brandService.updateById(brandEntity);
-
-        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",6L));
-        list.forEach((item)->{
-            System.out.println(item);
-        });
-    }
+//    @Autowired
+//    OSS ossClient;
+//
+//    @Test
+//    public void testUpload() throws Exception {
+////        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+////        String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
+////        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//////        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+////
+////        // RAM用户的访问密钥（AccessKey ID和AccessKey Secret）。
+////        String accessKeyId = "LTAI5tSG6mJJkxCQWPESCA3W";
+////        String accessKeySecret = "CXdIrmxZMUxPH71cnzbNBoFV8awN2E";
+////        // 使用代码嵌入的RAM用户的访问密钥配置访问凭证。
+////        CredentialsProvider credentialsProvider = new DefaultCredentialProvider(accessKeyId, accessKeySecret);
+////
+////        // 创建OSSClient实例。
+////        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+//
+//        // 填写Bucket名称，例如examplebucket。
+//        String bucketName = "mall-ningdong";
+//        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+//        String objectName = "key.txt";
+//        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+//        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+//        String filePath = "C:\\Users\\ningd\\Downloads\\key.txt";
+//
+//        try {
+//            InputStream inputStream = new FileInputStream(filePath);
+//            // 创建PutObjectRequest对象。
+//            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+//            // 创建PutObject请求。
+//            PutObjectResult result = ossClient.putObject(putObjectRequest);
+//        } catch (OSSException oe) {
+//            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+//                    + "but was rejected with an error response for some reason.");
+//            System.out.println("Error Message:" + oe.getErrorMessage());
+//            System.out.println("Error Code:" + oe.getErrorCode());
+//            System.out.println("Request ID:" + oe.getRequestId());
+//            System.out.println("Host ID:" + oe.getHostId());
+//        } catch (ClientException ce) {
+//            System.out.println("Caught an ClientException, which means the client encountered "
+//                    + "a serious internal problem while trying to communicate with OSS, "
+//                    + "such as not being able to access the network.");
+//            System.out.println("Error Message:" + ce.getMessage());
+//        } finally {
+//            if (ossClient != null) {
+//                ossClient.shutdown();
+//            }
+//        }
+//    }
 
 }
---
+++ mall-third-party/src/test/java/com/ningdong/mall_test/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,72 @@
+package com.ningdong.mall_test.thirdparty;
+
+import com.aliyun.oss.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    OSS ossClient;
+
+    @Test
+    public void testUpload() throws Exception {
+//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
+//        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+////        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+//
+//        // RAM用户的访问密钥（AccessKey ID和AccessKey Secret）。
+//        String accessKeyId = "LTAI5tSG6mJJkxCQWPESCA3W";
+//        String accessKeySecret = "CXdIrmxZMUxPH71cnzbNBoFV8awN2E";
+//        // 使用代码嵌入的RAM用户的访问密钥配置访问凭证。
+//        CredentialsProvider credentialsProvider = new DefaultCredentialProvider(accessKeyId, accessKeySecret);
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "mall-ningdong";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "test.txt";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath = "C:\\Users\\ningd\\Downloads\\key.txt";
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+            // 创建PutObject请求。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+}
